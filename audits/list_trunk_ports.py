b5b53dfc4cd7d6ad95b79ba16826ad7e5dc958a5
Superadm1n <kowalkyl@gmail.com>
2018-11-07 17:13:43 -0600

    Added scripts to list access and trunk ports configured on a device

---
+++ audits/list_trunk_ports.py
@@ -0,0 +1,38 @@
+"""
+MIT License
+
+Copyright (c) 2018 Kyle Kowalczyk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+Script to list all interfaces configured as a trunk port
+"""
+import os
+import sys
+script_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_path)
+import base
+
+t = base.Base()
+cisco_cfg = t.setUp()
+
+
+for x in cisco_cfg.find_objects_w_child(parentspec=r'^interf', childspec=r'mode trunk'):
+    print(x.text.split(' ')[-1])
