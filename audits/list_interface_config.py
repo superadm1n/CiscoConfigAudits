d9d8279a354b22f7e3fc655af58548d2de8d422e
Superadm1n <kowalkyl@gmail.com>
2018-11-13 14:47:08 -0600

    Modified script to allow for spaces in the interface name when surrounded by quotes

--- audits/list_interface_config.py
+++ audits/list_interface_config.py
@@ -43,7 +43,7 @@ def parse_input(input):
         else:
             interface_name += letter
         x += 1
-    return interface_name, interface_number
+    return interface_name.strip(' '), interface_number.strip(' ')
 
 app = base.CLIApp()
 app.add_argument('interface', help='Interface to view the configuration of ex. gi1/0/1 or GigabitEthernet1/0/1', type=str)
