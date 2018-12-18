e4b3280d3a7f66df3345459ab7b6efc9df3ded85
Superadm1n <kowalkyl@gmail.com>
2018-12-18 13:08:02 -0600

    Changed the format of how the listing monitor sessions script displays data to the user

--- audits/list_monitor_sessions.py
+++ audits/list_monitor_sessions.py
@@ -30,9 +30,46 @@ script_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_path)
 import base
 
+def check_list_for_session(list_of_dicts, session_id):
+    '''
+    Checks if a session is already listed in a dictionary
+
+    :param list_of_dicts: {'id': 1, 'source': 'addr', 'destination': 'addr'}, etc]
+    :param session_number: Session ID you are looking for
+    :return: True/False
+    '''
+    if len([item for item in list_of_dicts if item["id"] == session_id]) == 0:
+        return False
+    else:
+        return True
+
+def parse_monitor_session_text(monitor_sessions):
+    organized_sessions = []
+    for x in monitor_sessions:
+        ses_id = x.text.split()[2]
+        source_dest = x.text.split()[3]
+        interface = x.text.split()[-1]
+        if check_list_for_session(organized_sessions, ses_id) is True:
+            for y in organized_sessions:
+                if y['id'] == ses_id:
+                    y[source_dest] = interface
+        else:
+            organized_sessions.append({'id': ses_id, source_dest: interface})
+    return organized_sessions
+
 t = base.CLIApp()
 cisco_cfg = t.setUp()
 
+# Gets config lines for the port monitor sessions
 monitor_sessions = cisco_cfg.find_objects(r'^monitor session')
-for x in monitor_sessions:
-    print(x.text)
+# organizes raw config text into dictionary
+sessions = parse_monitor_session_text(monitor_sessions)
+# organizes the dictionary into a list of lists that can be printed out in columns
+printable_list = [[x['id'], x['source'], x['destination']] for x in sessions]
+# Adds a header for the list
+printable_list.insert(0, ['Session #', 'Source Port', 'Destination Port'])
+
+# prints the data to the user
+col_width = max(len(word) for row in printable_list for word in row) + 2  # padding
+for row in printable_list:
+    print("".join(word.ljust(col_width) for word in row))
